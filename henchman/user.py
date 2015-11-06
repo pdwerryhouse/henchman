90645e1eee2e504487bc98feb36f92b95af178bb
paul <paul@dwerryhouse.com.au>
2015-11-06 16:19:43 +1100

    User absent

--- henchman/user.py
+++ henchman/user.py
@@ -1,17 +1,33 @@
 
 from henchman.rule import RuleType
+import pwd
+import os.system
 
 class UserType(RuleType):
 
     __named_attribute__ = 'name'
 
+    def create(self):
+        try:
+            user = pwd.getpwnam(self.params.get('name'))
+        except KeyError as e:
+            user = None
+        
+    def remove(self):
+        try:
+            user = pwd.getpwnam(self.params.get('name'))
+        except KeyError as e:
+            return
+
+        os.system("userdel %s" % (self.params.get('name')))
+
     def operate(self):
 
         if self.params.get('ensure') in (None, "present"):
-            pass
+            self.create()
 
         elif self.params.get('ensure') == 'absent':
-            pass
+            self.remove()
 
 def User(name, **kwargs):
     rules.add(UserType(name,**kwargs))
