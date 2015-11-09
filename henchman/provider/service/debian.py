d2498a6756457aa9c4d36747b0cb5d7f5be01bea
paul <paul@dwerryhouse.com.au>
2015-11-09 22:22:01 +1100

    Missing brackets

--- henchman/provider/service/debian.py
+++ henchman/provider/service/debian.py
@@ -3,8 +3,8 @@ import os
 
 def enable(params):
     name = params.get('name')
-    os.system("/usr/sbin/update-rc.d %s enable" % (name,)
+    os.system("/usr/sbin/update-rc.d %s enable" % (name,))
 
 def disable(params):
     name = params.get('name')
-    os.system("/usr/sbin/update-rc.d %s disable" % (name,)
+    os.system("/usr/sbin/update-rc.d %s disable" % (name,))
